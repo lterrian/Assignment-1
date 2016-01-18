f2c3e4b069f20d67f455a4abfa3393757b0c964f
lterrian <lterrian@outlook.com>
2016-01-18 11:53:32 -0500

    Odd Numbers

---
+++ ConsoleApplication2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int x = 0; x <= 100; x++)
+                if (x % 2 != 0)
+                {
+                    Console.WriteLine(x);
+                }
+
+        }
+    }
+}
